0c1ca79498475a2a7c9b946a2e2fd9cac46ed79a
cooler-SAI <cooler19860212@yahoo.com>
2019-04-28 01:14:03 +0300

    Revert "fix error"
    This reverts commit 1516320e4c90b3e713df4b420abd6a7107aeb8a4.

--- src/server/game/Spells/SpellInfo.h
+++ src/server/game/Spells/SpellInfo.h
@@ -684,8 +684,6 @@ class TC_GAME_API SpellInfo
         bool CanSpellProvideImmunityAgainstAura(SpellInfo const* auraSpellInfo) const;
         bool SpellCancelsAuraEffect(AuraEffect const* aurEff) const;
 
-        bool IsAreaTriggerAuraEffect(uint32 difficulty, bool onlyplayer) const;
-
     private:
         // loading helpers
         void _InitializeExplicitTargetMask();
